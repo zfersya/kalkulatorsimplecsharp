36db3641b0967c133c40665972bf26b4d87d7a5e
zfersya <zfersya@gmail.com>
2024-05-02 22:00:45 +0700

    first commit

---
+++ test/test/Program.cs
@@ -0,0 +1,48 @@
+﻿using kalkulatorLibrary;
+using System;
+
+namespace kalkutor
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkulator 2 Digit Paling Legit sejagat raya");
+            Console.WriteLine(" ");
+            Console.WriteLine("Masukkan angka pertama\t:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masukkan angka kedua\t:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masukkan Operasi ( + , -, *, / , % )");
+            char operasi = Convert.ToChar(Console.ReadLine());
+            double hasil = 0;
+            if (operasi == '+')
+            {
+                hasil = point.Penambahan(num1, num2);
+            }
+            else if(operasi == '-')
+            {
+                hasil = point.Pengurangan(num1, num2);
+            }
+            else if (operasi == '*')
+            {
+                hasil = point.Perkalian(num1, num2);
+            }
+            else if(operasi == '/')
+            {
+                hasil = point.Pembagian(num1, num2);
+            }
+            else if(operasi == '%')
+            {
+                hasil = point.Modulo(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Hasil\t:" + hasil);
+
+        }
+    }
+}
---
+++ testLibrary/testLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace kalkulatorLibrary
+{
+    public static class point
+    {
+        public static double Penambahan(double num1, double num2)
+        {
+            return (num1 + num2);
+        }
+        public static double Pengurangan(double num1, double num2)
+        {
+            return (num1 - num2);
+        }
+        public static double Perkalian(double num1, double num2)
+        {
+            return (num1 * num2);
+        }
+        public static double Pembagian(double num1, double num2)
+        {
+            if(num2 == 0) 
+            { 
+                throw new DivideByZeroException("Tidak Bisa Nol");
+            }
+            return (num1 / num2);
+        }
+        public static double Modulo(double num1, double num2)
+        {
+            return (num1 % num2);
+        }
+    }
+}
